60b6cbd0f30a92008dff3a93c1135348cb03d3ab
Yana Kalsina <yaprogrammer18@gmail.com>
2026-03-31 13:22:49 +0300

    Revert "renamed modules with files"
    This reverts commit 6bddf2e5cbde0a8c82b6d0c12d12efffeb2613eb.

--- src/fileWork.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "minHeap.h"
-
-typedef struct Containe Container;
-Container* createContainer(MinHeap* heap, size_t qos);
-size_t blockAnalyzer(char* data, size_t len, MinHeap* heap);
