50a9ef40ba11b99fc68c375194905c4adc190cbf
Yana Kalsina <yaprogrammer18@gmail.com>
2026-06-11 12:55:43 +0300

    Add function describtion in fileReader.h file

--- src/fileReader.h
+++ src/fileReader.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "minHeap.h"
 
+// Функция сжатия.
+// На вход принимает входной файл и файл, куда пользователь хочет, чтобы она записала сжатые данные
 void fileCompressAndWrite(char* inputFilepath, char* outputFilepath);
+
+// Функция расжатия.
+// На вход принимает входной файл со сжатыми данными и файл, куда пользователь хочет, чтобы она записала расжатые данные
 void fileDecompressAndWrite(char* compressedFilepath, char* outputFile);
